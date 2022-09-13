d0d0d0bc2c05123c7b3a613fe9803a09c50f7301
Aino <n1huai00@students.oamk.fim>
2022-09-13 16:54:41 +0300

    Lisäys

--- alcometer/src/App.js
+++ alcometer/src/App.js
@@ -42,6 +42,7 @@ function App() {
       <div>
       <label>Bottles:</label>
       <select name="bottles" value={bottles} onChange={e => setBottles(e.target.value)}>
+        <option value="0">0</option>
         <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
